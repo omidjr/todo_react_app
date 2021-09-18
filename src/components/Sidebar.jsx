60a2b56256ebaf69f63f3b5cf800b28f0b4c8d31
Omid Jahandar <omid.jahandar.98@gmail.com>
2021-09-18 14:42:13 +0430

    Added collection add form

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,6 +2,10 @@ import "./Sidebar.css";
 import React from 'react'
 
 const Sidebar = ({showSidebar}) => {
+    const formSubmitted = () => {}
+
+    const onNewCollectionChange = (e) => {}
+
     return (
         <div className={showSidebar ? ['sideBar', 'active'].join(' ') : "sideBar"}>
             <p>
@@ -69,6 +73,23 @@ const Sidebar = ({showSidebar}) => {
                     School
                 </li>
             </ul>
+            <form className="addCollection_form" onSubmit={formSubmitted}>
+                <input
+                    className="addCollection_input"
+                    autoComplete="off"
+                    placeholder="Add a Collection..."
+                    id="newCollection"
+                    name="newCollection"
+                    // value={newCollection}
+                    onChange={onNewCollectionChange}
+                />
+                <button className="addCollection_btn">
+                    <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M7.87097 1.19354V13.0645" stroke="#EEEEEE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M1.93548 7.12903H13.8064" stroke="#EEEEEE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                </button>
+            </form>
         </div>
     )
 }
