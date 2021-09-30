b53606654a2f102773d8aaeec5eed8b0c083d8ab
Omid Jahandar <omid.jahandar.98@gmail.com>
2021-09-30 18:52:07 +0330

    Added modal close on outside click

--- src/components/EditTodoModal.jsx
+++ src/components/EditTodoModal.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useState, useRef, useEffect } from "react";
 import "./EditTodoModal.css";
 
 const EditTodoModal = ({setModal, chosenTodo}) => {
@@ -15,8 +15,23 @@ const EditTodoModal = ({setModal, chosenTodo}) => {
         setModal(false);
     }, [editedTodo]);
 
+    let modalRef = useRef();
+
+    useEffect(() => {
+        let handler = (e) => {
+            if (!modalRef.current.contains(e.target)) {
+                setModal(false);
+            }
+        };
+        document.addEventListener("mousedown", handler);
+        
+        return () => {
+            document.removeEventListener("mousedown", handler);
+        }
+    })
+
     return (
-            <div className="center popup"> 
+            <div ref={modalRef} className="center popup"> 
                 <div className="editIcon">
                     <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M0 17.416V22H4.58397L18.1097 8.47423L13.5258 3.89026L0 17.416ZM21.6425 4.94152C22.1192 4.46479 22.1192 3.68857 21.6425 3.21184L18.7882 0.35755C18.3114 -0.119183 17.5352 -0.119183 17.0585 0.35755L14.8215 2.59453L19.4055 7.1785L21.6425 4.94152Z" fill="#E94560"/>
